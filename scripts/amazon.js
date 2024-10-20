45db09a176d8c21ec521cbd79ad4b0e5191ace3f
Divine <divinenamirembe@gmail.com>
2024-10-20 18:56:44 +0300

    export and import between 2 files

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,4 @@
-import {cart, addToCart} from '../data/cart.js';
+import {cart, addToCart, calculateCartQunatity} from '../data/cart.js';
 import {products} from '../data/products.js';
 import {formatCurrency} from './utils/money.js';
 
@@ -65,15 +65,13 @@ products.forEach((product) => {
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
 function updateCartQuantity() {
-  let cartQuantity = 0;
+  
+  const cartQuantity = calculateCartQuantity();
 
-  cart.forEach((cartItem) => {
-    cartQuantity += cartItem.quantity;
-  });
-
-  document.querySelector('.js-cart-quantity').innerHTML = cartQuantity
-
+  document.querySelector('.js-cart-quantity')
+    .innerHTML = cartQuantity;
 }
+
 
 updateCartQuantity();
 
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import {cart, removeFromCart} from '../data/cart.js';
+import {cart, removeFromCart, calculateCartQuantity} from '../data/cart.js';
 import {products} from '../data/products.js'
 import {formatCurrency} from './utils/money.js';
 
@@ -118,12 +118,9 @@ document.querySelectorAll('.js-delete-link')
     });
   });
 
-  function updateCartQuantity () {
-    let cartQuantity = 0;
-
-    cart.forEach((cartItem) => {
-      cartQuantity += cartItem.quantity;
-    });
+  function updateCartQuantity() {
+   
+    const cartQuantity = calculateCartQuantity();
     
     document.querySelector('.js-return-to-home-link')
       .innerHTML = `${cartQuantity} items`;
