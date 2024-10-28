8b94a03f2a0e294be60204183f839c0f63b5bdf3
Divine <divinenamirembe@gmail.com>
2024-10-28 20:15:57 +0300

    Save data and generate HTML for payment summary

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,8 +1,10 @@
 import {renderOrderSummary} from './Checkout/orderSummary.js';
+import {renderPaymentSummary} from './Checkout/paymentSummary.js'
 
 renderOrderSummary();
+renderPaymentSummary();
 
 
 //console.log('Products:', products);
 //console.log('Cart:', cart);
-console.log('Delivery Options:', deliveryOptions);
+//console.log('Delivery Options:', deliveryOptions);
